18d38da88362745ce49e45010eb1340138dfde8e
abinaya <abinayavidhya315@gmail.com>
2023-03-15 17:51:20 +0530

    deleted scenario outline

--- hyderabad/src/test/java/StepDefinition/FlightsList.java
+++ hyderabad/src/test/java/StepDefinition/FlightsList.java
@@ -1,11 +1,15 @@
 package StepDefinition;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class FlightsList {
 
@@ -18,15 +22,30 @@ public class FlightsList {
 	   
 	}
 
-	@And("I type url {}")
-	public void i_type_url(String website) {
+	@When("I type url {}")
+	public void i_type_url(String website) throws InterruptedException {
 	   driver.get(website);
 	   driver.manage().window().maximize();
+	   Thread.sleep(3000);
 	}
 
-	@Then("I select depart from destination")
-	public void i_select_depart_from_destination() {
-	   
+	@Then("I close popup")
+	public void i_close_popup(){
+		Alert a=driver.switchTo().alert();
+		a.dismiss();
+		
+	}
+	@Then("I select {} from {} to")
+	
+	public void i_select_depart_from_destination(String dep, String destination) {
+		
+	   WebElement textbox=driver.findElement(By.xpath("(//span[text()='Depart From'])[1]"));
+	 textbox.click();
+	 textbox.sendKeys(dep);
+	 
+	 WebElement textbox2=driver.findElement(By.xpath("(//span[text()='Going To'])[1]"));
+	 textbox2.click();
+	 textbox2.sendKeys(destination);
 	}
 
 	@And("I select date")
--- hyderabad/src/test/resources/Features/login.feature
+++ hyderabad/src/test/resources/Features/login.feature
@@ -6,16 +6,17 @@
 Feature: Take the list of flights from home to destination on particular date
 
 @SmokeTest
-  Scenario: Creating a scenario for ramdom list
-Given I open browser 
-And I type url https://www.yatra.com/
-Then I select depart from destination to
-And I select date
-Then I click search flights
+#  Scenario: Creating a scenario for ramdom list
+#Given I open browser 
+#And I type url https://www.yatra.com/
+#Then I select depart from destination to
+#And I select date
+#Then I click search flights
 
   Scenario Outline: Creating a scenario for ramdom list
 Given I open browser 
-And I type url https://www.yatra.com/
+When I type url https://www.yatra.com/
+Then I close popup
 Then I select <depart> from <destination> to
 And I select date
 Then I click search flights
@@ -23,6 +24,7 @@ Then I click search flights
  Examples:
  | depart  | destination |
  | chennai | hyderabad   |
+ | mumbai  | delhi       |
  
 
 
